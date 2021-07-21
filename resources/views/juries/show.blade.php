536f33ec1c662ef1dfacc8224490cf17dd27b8da
Ibrahima Sarr <wpibousarr@gmail.com>
2021-07-21 23:44:30 +0000

    Correctifs essentiels

--- resources/views/juries/show.blade.php
+++ resources/views/juries/show.blade.php
@@ -1,7 +1,7 @@
-<x-premiertour-layout>
+<x-jury-layout>
 
     <x-slot name="header">
-        <h2 class="flex items-center w-full rounded-lg font-semibold text-2xl text-gray-300 p-2 font-bold leading-tight text-left bg-green-700">
+        <h2 class="flex items-center w-full p-2 text-2xl font-semibold font-bold leading-tight text-left text-gray-300 bg-green-700 rounded-lg">
             <div class="w-3/5">
                  Settings du jury 
             </div>
@@ -11,108 +11,108 @@
     </x-slot>
 
     <div class="w-9/12 p-1 mx-auto mt-5 bg-gray-100 shadow-3xl">
-        <div class="flex flex-1  flex-col md:flex-row lg:flex-row mx-2">
-            <div class="mb-2 border-solid border-red-300 rounded border shadow-sm w-full">
-                <div class="border-red-900 bg-blue-700 text-white font-bold px-2 p-1 border-solid border-gray-300 border-green-600 text-3xl text-center">
+        <div class="flex flex-col flex-1 mx-2 md:flex-row lg:flex-row">
+            <div class="w-full mb-2 border border-red-300 border-solid rounded shadow-sm">
+                <div class="p-1 px-2 text-3xl font-bold text-center text-white bg-blue-700 border-gray-300 border-red-900 border-green-600 border-solid">
                     <div class="px-6 py-1">
-                        <div class="md:flex md:items-center mb-2">
+                        <div class="mb-2 md:flex md:items-center">
                         
                         </div>
                     </div>                    
                 </div>
-                <div class="border-blue-900 p-1 flex flex-1 flex-col md:flex-row lg:flex-row justify-between md:mx-2 lg:mx-2">
-                    <div class="mt-5 w-1/2 rounded overflow-hidden shadow-lg">
+                <div class="flex flex-col justify-between flex-1 p-1 border-blue-900 md:flex-row lg:flex-row md:mx-2 lg:mx-2">
+                    <div class="w-1/2 mt-5 overflow-hidden rounded shadow-lg">
                         <div class="px-6 py-1">
                             
-                            <div class="md:flex md:items-center mb-2">
+                            <div class="mb-2 md:flex md:items-center">
                                 <div class="md:w-1/2">
-                                    <p class="text-bold pb-2"><b>Session du BFEM</b></p>
+                                    <p class="pb-2 text-bold"><b>Session du BFEM</b></p>
                                 </div>
                                 <div class="md:w-1/2">
-                                    <p class="text-bold text-xl"><b>{{ $jury->session}}</b></p>
+                                    <p class="text-xl text-bold"><b>{{ $jury->session}}</b></p>
                                 </div>
                             </div>
                             
-                            <div class="md:flex md:items-center mb-2">
+                            <div class="mb-2 md:flex md:items-center">
                                 <div class="md:w-1/2">
-                                    <p class="text-bold text-xl pb-2"><b>Académie</b></p>
+                                    <p class="pb-2 text-xl text-bold"><b>Académie</b></p>
                                 </div>
                                 <div class="md:w-1/2">
-                                    <p class="text-bold text-xl pb-2"><b>{{ $jury->academie}}</b></p>
+                                    <p class="pb-2 text-xl text-bold"><b>{{ $jury->academie}}</b></p>
                                 </div>
                             </div>
                         </div>
                         <div class="px-6 py-1">
-                            <div class="md:flex md:items-center mb-2">
+                            <div class="mb-2 md:flex md:items-center">
                                 <div class="md:w-1/2">
-                                    <p class="text-bold text-xl pb-2"><b>I.E.F.</b></p>
+                                    <p class="pb-2 text-xl text-bold"><b>I.E.F.</b></p>
                                 </div>
                                 <div class="md:w-1/2">
-                                    <p class="text-bold text-xl pb-2"><b>{{ $jury->ief}}</b></p>
+                                    <p class="pb-2 text-xl text-bold"><b>{{ $jury->ief}}</b></p>
                                 </div>
                                 
                             </div>
                         </div>
                         <div class="px-6 py-1">
-                            <div class="md:flex md:items-center mb-2">
+                            <div class="mb-2 md:flex md:items-center">
                                 <div class="md:w-1/2">
-                                    <p class="text-bold text-xl pb-2"><b>Centre</b></p>
+                                    <p class="pb-2 text-xl text-bold"><b>Centre</b></p>
                                 </div>
                                 <div class="md:w-1/2">
-                                    <p class="text-bold text-xl pb-2"><b>{{ $jury->centre}}</b></p>
+                                    <p class="pb-2 text-xl text-bold"><b>{{ $jury->centre}}</b></p>
                                 </div>
                             </div>
                         </div>
                         <div class="px-6 py-1">
-                            <div class="md:flex md:items-center mb-2">
+                            <div class="mb-2 md:flex md:items-center">
                                 <div class="md:w-1/2">
-                                    <p class="text-bold text-xl pb-2"><b>Numéro</b></p>
+                                    <p class="pb-2 text-xl text-bold"><b>Numéro</b></p>
                                 </div>
                                 <div class="md:w-1/2">
-                                    <p class="text-bold text-xl pb-2"><b>{{ $jury->jury }}</b></p>
+                                    <p class="pb-2 text-xl text-bold"><b>{{ $jury->jury }}</b></p>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div class="mt-5 w-1/2 rounded overflow-hidden shadow-lg">
+                    <div class="w-1/2 mt-5 overflow-hidden rounded shadow-lg">
                         <div class="px-6 py-1">
-                            <div class="md:flex md:items-center mb-2">
+                            <div class="mb-2 md:flex md:items-center">
                                 <div class="md:w-1/2">
-                                    <p class="text-bold text-xl pb-2"><b>Président</b></p>
+                                    <p class="pb-2 text-xl text-bold"><b>Président</b></p>
                                 </div>
                                 <div class="md:w-1/2">
-                                    <p class="text-bold text-xl pb-2"><b>{{ $jury->president }}</b></p>
+                                    <p class="pb-2 text-xl text-bold"><b>{{ $jury->president }}</b></p>
                                 </div>
                             </div>
                         </div>
                         <div class="px-6 py-1">
-                            <div class="md:flex md:items-center mb-2">
+                            <div class="mb-2 md:flex md:items-center">
                                 <div class="md:w-1/2">
-                                    <p class="text-bold text-xl pb-2"><b>Grade</b></p>
+                                    <p class="pb-2 text-xl text-bold"><b>Grade</b></p>
                                 </div>
                                 <div class="md:w-1/2">
-                                    <p class="text-bold text-xl pb-2"><b>{{ $jury->gradePresident }}</b></p>
+                                    <p class="pb-2 text-xl text-bold"><b>{{ $jury->gradePresident }}</b></p>
                                 </div>
                                 
                             </div>
                         </div>
                         <div class="px-6 py-1">
-                            <div class="md:flex md:items-center mb-2">
+                            <div class="mb-2 md:flex md:items-center">
                                 <div class="md:w-1/2">
-                                    <p class="text-bold text-xl pb-2"><b>Matricule</b></p>
+                                    <p class="pb-2 text-xl text-bold"><b>Matricule</b></p>
                                 </div>
                                 <div class="md:w-1/2">
-                                    <p class="text-bold text-xl pb-2"><b>{{ $jury->matriculePresident }}</b></p>
+                                    <p class="pb-2 text-xl text-bold"><b>{{ $jury->matriculePresident }}</b></p>
                                 </div>
                             </div>
                         </div>
                         <div class="px-6 py-1">
-                            <div class="md:flex md:items-center mb-2">
+                            <div class="mb-2 md:flex md:items-center">
                                 <div class="md:w-1/2">
-                                    <p class="text-bold text-xl pb-2"><b></b></p>
+                                    <p class="pb-2 text-xl text-bold"><b></b></p>
                                 </div>
                                 <div class="md:w-1/2">
-                                    <p class="text-bold text-xl pb-2"><b></b></p>
+                                    <p class="pb-2 text-xl text-bold"><b></b></p>
                                 </div>
                             </div>
                         </div>
@@ -124,13 +124,13 @@
                 </div>
             </div>
         </div>
-        <div class="flex flex-1  flex-col md:flex-row lg:flex-row mx-2">
-            <div class="mb-2 border-solid border-red-300 rounded border shadow-sm w-full">
-                <div class="border-blue-900 p-1 flex flex-1 flex-col md:flex-row lg:flex-row justify-between md:mx-2 lg:mx-2">
+        <div class="flex flex-col flex-1 mx-2 md:flex-row lg:flex-row">
+            <div class="w-full mb-2 border border-red-300 border-solid rounded shadow-sm">
+                <div class="flex flex-col justify-between flex-1 p-1 border-blue-900 md:flex-row lg:flex-row md:mx-2 lg:mx-2">
                     
                 </div>
             </div>
         </div>
     </div>
 
-</x-premiertour-layout>
+</x-jury-layout>
